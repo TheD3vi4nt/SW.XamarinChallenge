4d46e47d41b860b84cbee3dcf4425c683daecb3b
Raul Vega <jvega@bside.com.mx>
2020-11-18 20:41:26 -0600

    Initial solution setup

---
+++ GoogleBooksChallenge/src/GoogleBooksChallenge.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using GoogleBooksChallenge.Core.ViewModels.Home;
+
+namespace GoogleBooksChallenge.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
